38c2641ec6b628be869f3a67444a368bdb4bcfb7
Daniel Robaina <dannyrobaina@gmail.com>
2017-11-03 20:51:33 -0400

    Log in button on side nav opens login activity

--- Feedly/app/src/main/java/com/sdpm/feedly/feedly/HomeNav.java
+++ Feedly/app/src/main/java/com/sdpm/feedly/feedly/HomeNav.java
@@ -1,5 +1,6 @@
 package com.sdpm.feedly.feedly;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -139,6 +140,11 @@ public class HomeNav extends AppCompatActivity implements ViewPager.OnPageChange
     @Override
     public void onPageScrollStateChanged(int state) {
 
+    }
+
+    public void openLoginView(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
     /**
