063be5d486e2949a1dbd055d6c0d358ca8af9933
Daniel Robaina <dannyrobaina@gmail.com>
2017-11-09 10:21:37 -0500

    added shared preference for theme so app can remember previous setting

--- Feedly/app/src/main/java/com/sdpm/feedly/utils/ChangeTheme.java
+++ Feedly/app/src/main/java/com/sdpm/feedly/utils/ChangeTheme.java
@@ -2,8 +2,12 @@ package com.sdpm.feedly.utils;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
 
 import com.sdpm.feedly.feedly.R;
+
+import static android.content.Context.MODE_PRIVATE;
 
 /**
  * Created by Dan on 11/9/2017.
@@ -16,12 +20,16 @@ public class ChangeTheme {
     public final static int THEME_MATERIAL_DARK = 1;
 
     public static void switchTheme(Activity activity) {
+        SharedPreferences userDetails = activity.getSharedPreferences("LoginInfo", MODE_PRIVATE);
+        SharedPreferences.Editor edit = userDetails.edit();
         if (sTheme == 0) {
             sTheme = 1;
         }
         else {
             sTheme = 0;
         }
+        edit.putInt("theme",sTheme);
+        edit.apply();
         activity.finish();
         activity.startActivity(new Intent(activity, activity.getClass()));
         activity.overridePendingTransition(android.R.anim.fade_in,
@@ -29,6 +37,8 @@ public class ChangeTheme {
     }
 
     public static void onActivityCreateSetTheme(Activity activity) {
+        SharedPreferences userDetails = activity.getSharedPreferences("LoginInfo", MODE_PRIVATE);
+        sTheme = userDetails.getInt("theme",0);
         switch (sTheme) {
             default:
             case THEME_MATERIAL_LIGHT:
